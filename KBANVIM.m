ef4daedf981294145a7c8226f16dd67940d08874
Sam Habiel <sam.habiel@gmail.com>
2014-07-11 15:51:28 -0700

    Added read only and auto xindex and UT upon save.

--- KBANVIM.m
+++ KBANVIM.m
@@ -1,9 +1,9 @@
-KBANVIM(RN) ; VEN/SMH - Use VIM Editor on Cache ; 4/29/14 4:51pm
+KBANVIM(RN,RO) ; VEN/SMH - Use VIM Editor on Cache ; 4/29/14 4:51pm
  G EN+1
  ;
-EN(RN) ; Public Entry point
+EN(RN,RO) ; Public Entry point
  LOCK +@RN:0 E  WRITE "Somebody else is editing this right now" QUIT
- ;
+ S RO=$G(RO,0)
  ;
  N POP
  D OPEN^%ZISH(RN,$$DEFDIR^%ZISH(),RN_".m","W")
@@ -19,30 +19,39 @@ EN(RN) ; Public Entry point
  D CLOSE^%ZISH(RN)
  ;
  ;
- N % S %=$ZF(-1,"vim "_$$DEFDIR^%ZISH()_RN_".m")
+ N % S %=$ZF(-1,"vim "_$S(RO:"-R ",1:"")_$$DEFDIR^%ZISH()_RN_".m")
  I %'=0 WRITE "Vim exited abnormally... quitting..."
  ;
  ;
  ;
- N NEWRTN
- D OPEN^%ZISH(RN,$$DEFDIR^%ZISH(),RN_".m","R")
- I POP WRITE "CANNOT OPEN FILE "_RN_" IN "_$$DEFDIR^%ZISH() QUIT
- N X,I S I=0
- USE IO
- F  R X:1  Q:$$STATUS^%ZISH()  S I=I+1,NEWRTN(I,0)=X
- D CLOSE^%ZISH(RN)
- ;
- ;
- ;
- K ^TMP($J) M ^TMP($J)=NEWRTN
- N DIE,X,XCN S DIE="^TMP($J,",X=RN,XCN=0
- X ^%ZOSF("SAVE")
- ;
- W "Saved routine "_RN,!
- ;
- N A S A(RN_".m")=""
- N % S %=$$DEL^%ZISH($$DEFDIR^%ZISH(),$NA(A))
- ;
- ;
+ I 'RO D
+ . N NEWRTN
+ . D OPEN^%ZISH(RN,$$DEFDIR^%ZISH(),RN_".m","R")
+ . I POP WRITE "CANNOT OPEN FILE "_RN_" IN "_$$DEFDIR^%ZISH() QUIT
+ . N X,I S I=0
+ . USE IO
+ . F  R X:1  Q:$$STATUS^%ZISH()  S I=I+1,NEWRTN(I,0)=X
+ . D CLOSE^%ZISH(RN)
+ . ;
+ . ;
+ . ;
+ . K ^TMP($J) M ^TMP($J)=NEWRTN
+ . N DIE,X,XCN S DIE="^TMP($J,",X=RN,XCN=0
+ . X ^%ZOSF("SAVE")
+ . ;
+ . W "Saved routine "_RN,!
+ . ;
+ . N A S A(RN_".m")=""
+ . N % S %=$$DEL^%ZISH($$DEFDIR^%ZISH(),$NA(A))
+ . ;
+ . K ^TMP($J)
+ . D 
+ . . N RTN S RTN=RN
+ . . N RN
+ . . D CHKROU^XTECROU($NA(^TMP($J)),RTN,RTN)
+ . . W !
+ . . N I F I=0:0 S I=$O(^TMP($J,I)) Q:'I  W ^(I),!
+ . ;
+ K ^TMP($J)
  LOCK -@RN
  QUIT
