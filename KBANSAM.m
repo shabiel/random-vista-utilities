c4114ceb416b2fd015d5445f10ef0f8e877a460b
Sam Habiel <sam.habiel@gmail.com>
2014-08-12 15:17:20 -0400

    Fixed Cache issue (%RD starts with 0 node)

--- KBANSAM.m
+++ KBANSAM.m
@@ -3,12 +3,12 @@ KBANSAM ; VEN/SMH - Private utilities ;2014-07-16  2:04 PM
 S(INPUT) ; Summarize Routine
  X ^%ZOSF("RSEL")
  N R S R=$NA(^UTILITY($J))
- Q:'$L($O(@R@(""))) ; don't do anything if no routines present
- N RTN S RTN="" ; Routine holder
+ I '$L($O(@R@(""))) QUIT  ; don't do anything if no routines present
+ N RTN S RTN=0 ; Routine holder
  N WM S WM=$C(27)_"[1;37;45m" ; Bright White on Magneta VT100 formatting
  N RES S RES=$C(27)_"[0m" ; Reset VT100 formatting
  F  S RTN=$O(@R@(RTN)) Q:'$L(RTN)  D
- . Q:'$L($T(+1^@RTN)) ; Quit if not present
+ . I '$L($T(+1^@RTN)) QUIT  ; Quit if not present
  . W WM,$T(+1^@RTN),RES,! ; Print first line. Fancy!!
  . ; Loop, grab each line, grab first character, print line if char isn't space or tab
  . N I,L,A F I=2:1 S L=$T(+I^@RTN) Q:'$L(L)  D
