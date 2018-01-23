87565ab4509d572304f6c20ff642b26b05385e74
Sam Habiel <sam.habiel@gmail.com>
2018-01-22 19:09:54 -0500

    Print Templates analysis

--- KBANDIEPBT.m
+++ KBANDIEPBT.m
@@ -1,4 +1,4 @@
-KBANDIEPBT ; OSE/SMH - Input, Print, and Sort Template Analysis;2018-01-22  12:47 PM
+KBANDIEPBT ; OSE/SMH - Input, Print, and Sort Template Analysis;2018-01-22  7:06 PM
  ;;0.1;OSEHRA;
  ;
  ; This routine finds non-self files that are pointed to by a template
@@ -15,6 +15,11 @@ DIET ; [Public] Input template analysis
  d DIETOUT(.outputData,"/tmp/","DIETOUT.csv")
  quit
  ;
+DIPT ; [Public] Print template analysis
+ n outputData
+ d DIPTCOL(.outputData)
+ d DIPTOUT(.outputData,"/tmp/","DIPTOUT.csv")
+ quit
  ; 
 DIBTCOL(outputData) ; [Private] Sort Template Data Collection
  ; for each template
@@ -38,10 +43,10 @@ DIBTCOL(outputData) ; [Private] Sort Template Data Collection
  .. n lineFieldSpec s lineFieldSpec=$p(lineData,U,3)
  .. ;
  .. ; if it's the same file, and not a relational field, we are not interested
- .. i lineFile=file,lineField'[":" quit
+ .. i lineFile=file,(lineFieldSpec'[":"&(lineFieldSpec'[" IN ")) quit
  .. ;
  .. ; if the parent is the same file, and ditto, we are still not interested
- .. i $$PARENT(lineFile)=file,lineField'[":" quit
+ .. i $$PARENT(lineFile)=file,(lineFieldSpec'[":"&(lineFieldSpec'[" IN ")) quit
  .. ;
  .. ; We are interested
  .. ; Do we have the field?
@@ -57,6 +62,7 @@ DIBTCOL(outputData) ; [Private] Sort Template Data Collection
  .... s pair=$p(X("USED"),";",pairs)
  .... n thisFile  s thisFile=$p(pair,U,1)
  .... n thisField s thisField=$p(pair,U,2)
+ .... i thisFile=file quit
  .... s outputData(file,thisFile,thisField)=dibt_U_name
  .. ;
  .. ; we have a field. Take it at face value
@@ -127,6 +133,121 @@ DIETOUT(outputData,outputPath,outputFile) ; [Private] Input Template Data Output
  n file,dstFile,dstField,dietIEN,dietName
  n c s c=","
  w "INPUT TEMPLATE IEN,INPUT TEMPLATE NAME,SOURCE FILE,DESTINATION FILE,DESTINATION FIELD",!
+ f file=0:0 s file=$o(outputData(file)) q:'file  d
+ . f dstFile=0:0 s dstFile=$o(outputData(file,dstFile)) q:'dstFile  d
+ .. f dstField=0:0 s dstField=$o(outputData(file,dstFile,dstField)) q:'dstField  d
+ ... n data s data=outputData(file,dstFile,dstField)
+ ... s dietIEN=$p(data,U,1)
+ ... s dietName=$p(data,U,2)
+ ... w dietIEN_c_dietName_c_file_c_dstFile_c_dstField,!
+ d CLOSE^%ZISH("file1")
+ quit
+ ;
+DIPTCOL(outputData) ; [Private] Print Template Data Collection
+ ; for each template
+ n dipt f dipt=0:0 s dipt=$o(^DIPT(dipt)) q:'dipt  d
+ . quit:'$data(^DIPT(dipt,0))                 ; get valid ones only
+ . new name s name=$p(^DIPT(dipt,0),U)
+ . new file s file=$p(^DIPT(dipt,0),U,4)
+ . ; for each field
+ . new fileNamePrint set fileNamePrint=1
+ . new line f line=0:0 s line=$o(^DIPT(dipt,"F",line)) q:'line  do
+ .. new lineContents s lineContents=^DIPT(dipt,"F",line)
+ .. new fieldDataIndex for fieldDataIndex=1:1:$l(lineContents,"~") do
+ ... new fieldData set fieldData=$p(lineContents,"~",fieldDataIndex)
+ ... quit:fieldData=""
+ ... n fields s fields=$p(fieldData,";")
+ ... ;
+ ... ; analyze the fields
+ ... ;
+ ... ; See if we have a multiple navigation. These are noted in the first piece
+ ... ; as a series of numbers like 50,1,2,5...
+ ... ; don't process these any further if we find them
+ ... n fieldsUpright s fieldsUpright=1
+ ... n fieldIndex f fieldIndex=1:1:$l(fields,",") do  q:'fieldsUpright
+ .... n field s field=$p(fields,",",fieldIndex)
+ .... i field'=+field!(field<0) s fieldsUpright=0
+ ... i fieldsUpright quit
+ ... ;
+ ... ; exclude print only fields
+ ... ; also find M code fields
+ ... n printOnlyField s printOnlyField=0
+ ... n fieldIsMCode s fieldIsMCode=0
+ ... n fieldIndex f fieldIndex=1:1:$l(fields,",") do  q:printOnlyField  q:fieldIsMCode
+ .... n field s field=$p(fields,",",fieldIndex)
+ .... n extField s extField=$p(fields,",",fieldIndex,99)
+ .... d  q:fieldIsMCode
+ ..... n X s X=extField D ^DIM
+ ..... i $D(X) s fieldIsMCode=1 d
+ ...... s fieldIsMCode("MCode")=extField
+ ...... s fieldIsMCode("nonMCode")=$p(fields,extField)
+ ...... s $e(fieldIsMCode("nonMCode"),$l(fieldIsMCode("nonMCode")))="" ; remove comma
+ .... ;
+ .... i $e(field)="""",$e(field,$l(field))="""" s printOnlyField=1
+ ... i printOnlyField quit
+ ... ;
+ ... ; Now, process non-M code fields
+ ... ; Best template to test this with: MAGV-PAT-QUERY
+ ... ; NB: This is a recursive search; each search updates the pointerFile variable
+ ... ; We only want the last entry in the pointerFile chain to file the data if there
+ ... ; is a field we want to grab
+ ... n pointerFile s pointerFile=0
+ ... n ignoreTransition s ignoreTransition=0
+ ... if 'fieldIsMCode n fieldIndex f fieldIndex=1:1:$l(fields,",") do  q:ignoreTransition
+ .... n field s field=$p(fields,",",fieldIndex)
+ .... n nextField s nextField=$p(fields,",",fieldIndex+1)
+ .... i field<0 s pointerFile=-field
+ .... i $e(nextField)=U set ignoreTransition=1 quit
+ .... i field>0,'pointerFile quit
+ .... i field>0,pointerFile s outputData(file,pointerFile,field)=dipt_U_name
+ ... ;
+ ... ;
+ ... ; Now, process M code fields.
+ ... if fieldIsMCode do
+ .... ; The file number for the M code operation
+ .... n mCodeContext s mCodeContext=$p(fieldIsMCode("nonMCode"),",",$l(fieldIsMCode("nonMCode"),","))
+ .... i mCodeContext="" s mCodeContext=file
+ .... i mCodeContext<0 s mCodeContext=-mCodeContext ; switch sign if relational jump (which it always is in this context)
+ .... n Zpiece
+ .... n i f i=1:1:$l(fieldData,";") i $p(fieldData,";",i)="Z" s Zpiece=i quit
+ .... i '$get(Zpiece) quit  ; field does not have definition (e.g. CAPTIONED template)
+ .... ;
+ .... ; Get the potentially COMPUTED EXPRESSION code for this field
+ .... n potComputedCode s potComputedCode=$p(fieldData,";",Zpiece+1)
+ .... s potComputedCode=$e(potComputedCode,2,$l(potComputedCode)-1)
+ .... ;
+ .... ; Is it the same (after removing the quotes) as the MCode?
+ .... ; If so, then this is not a computed expression
+ .... ; We can abandon hope of finding what field it refers to.
+ .... i potComputedCode=fieldIsMCode("MCode") quit
+ ....
+ .... ; At this point, we think it's a computed expression.
+ .... ; Lets try to to see 
+ .... n X
+ .... d EXPR^DICOMP(mCodeContext,"dmFITSL",potComputedCode)
+ .... i '$d(X) w "Can't resolve "_fieldData_" into fields",! quit
+ .... i X("USED")="" quit  ; not an expression that uses fields (NOW, PAGE)
+ .... ;
+ .... n pairs,pair f pairs=1:1:$l(X("USED"),";") d
+ ..... s pair=$p(X("USED"),";",pairs)
+ ..... n thisFile  s thisFile=$p(pair,U,1)
+ ..... n thisField s thisField=$p(pair,U,2)
+ ..... i thisFile=file quit
+ ..... s outputData(file,thisFile,thisField)=dipt_U_name
+ ... ; 
+ ... ; i fileNamePrint w file," ",name,! s fileNamePrint=0
+ ... ; write fieldData,!
+ . ; i 'fileNamePrint zwrite:$d(outputData) outputData(file,*) w !!
+ quit
+ ;
+DIPTOUT(outputData,outputPath,outputFile) ; [Private] Print Template Data Output
+ n POP
+ d OPEN^%ZISH("file1",outputPath,outputFile,"W")
+ i POP quit
+ u IO
+ n file,dstFile,dstField,dietIEN,dietName
+ n c s c=","
+ w "PRINT TEMPLATE IEN,PRINT TEMPLATE NAME,SOURCE FILE,DESTINATION FILE,DESTINATION FIELD",!
  f file=0:0 s file=$o(outputData(file)) q:'file  d
  . f dstFile=0:0 s dstFile=$o(outputData(file,dstFile)) q:'dstFile  d
  .. f dstField=0:0 s dstField=$o(outputData(file,dstFile,dstField)) q:'dstField  d
